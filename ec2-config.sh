773375ed2550659e0320017bface71f75a7a8c23
Aldora <aldora988@gmail.com>
2024-06-13 22:21:33 -0700

    Update ec2-config

--- ec2-config.sh
+++ ec2-config.sh
@@ -5,3 +5,22 @@ sudo yum install git-2.39.2
 
 echo "===> install nginx"
 sudo yum install nginx-1.22.1
+
+echo "===> install npm"
+sudo yum install npm-8.19.2
+
+echo "===> install docker"
+sudo yum install docker-20.10.17
+
+echo "===> install docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/download/v2.15.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "===> install certbot"
+sudo dnf install -y augeas-libs
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot certbot-nginx
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
+
