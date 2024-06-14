d394de289a9ede71e1ea3e007675b772a9aad946
Aldora <aldora988@gmail.com>
2024-06-13 22:37:03 -0700

    Update ec2-config

--- ec2-config.sh
+++ ec2-config.sh
@@ -5,6 +5,7 @@ sudo yum install git-2.39.2
 
 echo "===> install nginx"
 sudo yum install nginx-1.22.1
+sudo systemctl start nginx.service
 
 echo "===> install npm"
 sudo yum install npm-8.19.2
@@ -13,7 +14,7 @@ echo "===> install docker"
 sudo yum install docker-20.10.17
 
 echo "===> install docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/v2.15.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "===> install certbot"
@@ -24,3 +25,7 @@ sudo /opt/certbot/bin/pip install certbot certbot-nginx
 sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 
 
+echo "===> install crontab"
+sudo yum install cronie
+sudo systemctl start crond.service
+sudo systemctl enable crond.service
