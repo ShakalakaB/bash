29c6b765e096d9a4220b529fe16698b57b0decd3
Aldora <aldora988@gmail.com>
2024-07-17 14:01:05 -0700

    Edit ec2-config

--- ec2-config.sh
+++ ec2-config.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
-echo "===> Install git"
+echo "================>  Install git"
 sudo yum install git-2.40.1
 
-echo "===> Install Nginx"
+echo "================>  Install Nginx"
 sudo yum install nginx-1.22.1
 sudo systemctl start nginx.service
 
-echo "===> Install nvm(node)"
+echo "================>  Install nvm(node)"
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
 nvm install 18.20.3
 
-echo "===> Enable Corepack"
+echo "================>  Enable Corepack"
 sudo npm update corepack -g
 corepack enable
 
-echo "===> Install Docker"
+echo "================>  Install Docker"
 sudo yum install docker-25.0.3
 
-echo "===> Install docker-compose"
+echo "================>  Install docker-compose"
 sudo curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "===> Install certbot"
+echo "================>  Install certbot"
 sudo dnf install -y augeas-libs
 sudo python3 -m venv /opt/certbot/
 sudo /opt/certbot/bin/pip install --upgrade pip
 sudo /opt/certbot/bin/pip install certbot certbot-nginx
 sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 
-echo "===> Install crontab"
+echo "================>  Install crontab"
 sudo yum install cronie
 sudo systemctl start crond.service
 sudo systemctl enable crond.service
 
 # Choose download package from "https://dev.mysql.com/downloads/"
-echo "===> Install MySQL client"
+echo "================>  Install MySQL client"
 sudo yum install -y https://dev.mysql.com/get/mysql84-community-release-el9-1.noarch.rpm
 sudo yum install -y mysql-community-client
 
+# Install pm2
+echo "================> Install MySQL client"
+sudo yarn global add pm2@5.4.2
 
 # set github ssh key
 # create ssh/config file
